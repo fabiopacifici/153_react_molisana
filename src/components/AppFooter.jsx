5853f583047b7262756275adbb3a3bf8cc930e81
fabiopacifici <boolean@fabiopacifici.com>
2026-03-06 11:45:00 +0100

    Enhance footer component with logo and navigation links

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,10 +1,62 @@
+import molisanaLogo from '/logo.png'
+
 export default function AppFooter() {
 
 
   return (
 
-    <footer>
-      Footer heres
+    <footer className='py-5'>
+
+      <div className="container">
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+          <div className="col">
+            <img src={molisanaLogo} alt="" />
+          </div>
+          <div className="col">
+            <h3 className='text-uppercase'>Pastificio</h3>
+            <ul className="list-unstyled">
+              <li>
+                <a href="" className='nav-link'>Il pastificio</a>
+              </li>
+              <li>
+                <a href="" className='nav-link'>Il pastificio</a>
+              </li>
+              <li>
+                <a href="" className='nav-link'>Il pastificio</a>
+              </li>
+              <li>
+                <a href="" className='nav-link'>Il pastificio</a>
+              </li>
+              <li>
+                <a href="" className='nav-link'>Il pastificio</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col">
+            <h3 className='text-uppercase'>ProDOTTI</h3>
+            <ul className="list-unstyled">
+              <li>
+                <a href="" className='nav-link'>Il pastificio</a>
+              </li>
+              <li>
+                <a href="" className='nav-link'>Il pastificio</a>
+              </li>
+              <li>
+                <a href="" className='nav-link'>Il pastificio</a>
+              </li>
+              <li>
+                <a href="" className='nav-link'>Il pastificio</a>
+              </li>
+              <li>
+                <a href="" className='nav-link'>Il pastificio</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+
+
     </footer>
 
   )
