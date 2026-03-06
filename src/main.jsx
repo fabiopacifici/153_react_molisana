babba17f8e7d493c12710d00d36507c0e599b99b
fabiopacifici <boolean@fabiopacifici.com>
2026-03-06 11:33:41 +0100

    Add bootstrap dependencies and update stylings

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // Supports weights 100-900
 import '@fontsource-variable/raleway';
+import * as bootstrap from 'bootstrap'
 import './index.css'
 import App from './App.jsx'
 
